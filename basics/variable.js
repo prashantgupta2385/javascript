d328540ea7aecdefd0db299942539708f96da07d
prashantgupta2385 <160148906+prashantgupta2385@users.noreply.github.com>
2024-02-15 17:50:59 +0000

    configure done

---
+++ basics/variable.js
@@ -0,0 +1,14 @@
+const accountId=1234
+var accountPassword="12345"
+let accountEmail="abc.gmail.com"
+account_city="noida"
+let state;
+var phoneNumber=912345567
+//x1=ram// not valid
+//2x="sh" // not  valid 
+x2='c'
+// console.log(x1)
+
+console.log(x2)
+console.table([accountId,accountPassword,accountEmail,account_city,phoneNumber, state])
+
