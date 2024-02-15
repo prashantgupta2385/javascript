ae62b8f8d4a8c2b51a48ca2e5c6163ecf9d2be44
prashantgupta2385 <160148906+prashantgupta2385@users.noreply.github.com>
2024-02-15 18:02:02 +0000

    configure done

---
+++ basics/datatypes.js
@@ -0,0 +1,16 @@
+"use strict";//treat all js code as newer version
+//alert(4+4)//it is nodejs mot browser
+const accountId=1234
+var accountPassword="12345"
+let accountEmail="abc.gmail.com"
+account_city="noida"
+let state='c';
+var phoneNumber=912345567
+console.log(typeof accountId)
+console.log(typeof accountPassword)
+console.log(typeof accountEmail)
+console.log(typeof account_city)
+console.log(typeof account_city)
+console.log(typeof phoneNumber)
+console.log(typeof state)
+console.table([accountId,accountPassword,accountEmail,account_city,phoneNumber, state])
