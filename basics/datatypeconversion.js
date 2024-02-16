455107d3e4924b71cd5d29c59231a9edc1003e67
prashantgupta2385 <160148906+prashantgupta2385@users.noreply.github.com>
2024-02-16 13:44:48 +0000

    enter

---
+++ basics/datatypeconversion.js
@@ -0,0 +1,16 @@
+// let a='3'
+// let b=Number(a)
+// console.log(b)
+// console.log(typeof(b))
+// let a=3
+// let b=String (a)
+// console.log(b)
+// console.log(typeof(b))
+// let a="32asd"
+// let b=Number (a)
+// console.log(b)
+// console.log(typeof(b))
+let a=-1
+let b=Boolean (a)
+console.log(b)
+console.log(typeof(b))
