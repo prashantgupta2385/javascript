8f8792dddc1a59680bfd8333d5dcc5909944ad4f
prashantgupta2385 <160148906+prashantgupta2385@users.noreply.github.com>
2024-02-18 13:27:39 +0000

    operators in js

---
+++ basics/operators.js
@@ -0,0 +1,26 @@
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+// console.log(2**2)
+// let x=5
+// let y=x++
+// console.log(`x: ${x},y:${y}`);
+// let x=3
+// let y='3'// "3"
+// console.log(x!==y)
+// console.log(+"");
+// alert(2+2) error
+// let x,y,z=5
+// // console.log(`${x},${y},${z}`);
+
+// console.log(null==0)//return false as == operator compares null to 0 which is obiosly not equal.
+// console.log(null>=0)//retrun true as >= symobol and all operator of comparison type trears null t0 zero
+// console.log(null<0)//false
+// console.log(`${x},${y},${z}`);
+
+// console.log(undefined==0)//return false 
+// console.log(undefined>=0)//retrun false 
+// console.log(undefined<0)//false
+console.log("2"==="3")
