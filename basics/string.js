ec095f327ce1320cc00e212d3787939c5f674c88
prashantgupta2385 <160148906+prashantgupta2385@users.noreply.github.com>
2024-03-03 11:26:49 +0000

    string done

---
+++ basics/string.js
@@ -0,0 +1,21 @@
+// ways to created string:
+// 1.here string is object type so it have many methods aytached with it;
+var s=new String("ram ji is idol of millions hindu");
+// console.log(s);
+// //2nd way:using string type
+// const b="ram";
+// console.log(typeof(b));
+// console.log(s==b)
+// console.log(b.charAt())
+// console.log(s.valueOf())
+// console.log(s.indexOf('a'));
+// console.log(s.toLowerCase());
+// console.log(s.toUpperCase());
+
+// console.log(s.concat(b));
+// // b=b+'s';
+// // console.log(b);
+// b.concat("ifjjejf")
+// s+="dwdw"
+// console.log(s);
+console.log(s.split(" "));
