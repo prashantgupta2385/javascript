a3ffd31fe53f9a0297440a633ffd5750833d88ac
prashantgupta2385 <160148906+prashantgupta2385@users.noreply.github.com>
2024-03-05 17:58:50 +0000

    maths complete

---
+++ basics/numberandmaths.js
@@ -0,0 +1,21 @@
+const a=100
+console.log(a);
+const  b=new Number(5);
+console.log(b);
+console.log(a.toPrecision(2));
+const salary =1000000
+console.log(salary.toExponential(10))
+console.log(salary.toLocaleString('en-In'))
+ // ________________________________________________ maths__________________________________________________________________________
+ console.log(Math.ceil(123.3))
+ console.log(Math.floor(123.3))
+ console.log(Math.round(123.3))
+ const min=1;
+ const max=6;
+ console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+ 
+
+
+
+
