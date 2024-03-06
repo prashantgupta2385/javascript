e76b1d2fd49ac50519491a3df87a9d227c9ca79c
prashantgupta2385 <160148906+prashantgupta2385@users.noreply.github.com>
2024-03-06 18:07:22 +0000

    dateand time completed

---
+++ basics/DATETIME.js
@@ -0,0 +1,11 @@
+let mydate=new Date("12-12-12");
+// console.log(mydate); 
+//  console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString("Deafault",{
+//     weekday:"long",calendar:"long"
+// }));
+console.log(mydate.getMonth()+1);
+console.log(mydate.getFullYear());
+console.log(mydate.getMilliseconds());
+console.log(Date.now());//in millisecond
+
