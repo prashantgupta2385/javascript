d8d5717e5d02df73347a17ae35ea746a52a08bdc
prashantgupta2385 <160148906+prashantgupta2385@users.noreply.github.com>
2024-03-10 10:48:38 +0000

    arrays

---
+++ basics/arrays.js
@@ -0,0 +1,22 @@
+// let S=new String(ram);
+// console.log(S);
+// S.toUpperCase();
+// console.log(S);
+// console.log(typeof S);
+// let a={
+//     name:"rohit"
+// }
+// console.log(a.name);
+// a.name.toUpperCase();
+// a.name="prasshant"
+// console.log(a.name);
+
+const arr=[0,1,2,3,4,5];
+// console.log(arr[4]);
+// console.log(arr);
+// arr[0]=5;
+// arr.push(0,10);
+console.log(arr);
+arr.pop();
+console.log(arr);
+
